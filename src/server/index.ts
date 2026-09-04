d9d1adc4865e1625b000f11da02e05a2f6476905
READTalk <redtalk.app@hotmail.com>
2026-09-05 02:25:14 +0700

    Implement Chat server with message handling

---
+++ src/server/index.ts
@@ -0,0 +1,86 @@
+import {
+	type Connection,
+	Server,
+	type WSMessage,
+	routePartykitRequest,
+} from "partyserver";
+
+import type { ChatMessage, Message } from "../shared";
+import authHandler from "./authorize";
+
+export class Chat extends Server<Env> {
+	static options = { hibernate: true };
+
+	messages = [] as ChatMessage[];
+
+	broadcastMessage(message: Message, exclude?: string[]) {
+		this.broadcast(JSON.stringify(message), exclude);
+	}
+
+	onStart() {
+		this.ctx.storage.sql.exec(
+			`CREATE TABLE IF NOT EXISTS messages (id TEXT PRIMARY KEY, user TEXT, role TEXT, content TEXT)`,
+		);
+
+		this.messages = this.ctx.storage.sql
+			.exec(`SELECT * FROM messages`)
+			.toArray() as ChatMessage[];
+	}
+
+	onConnect(connection: Connection) {
+		connection.send(
+			JSON.stringify({
+				type: "all",
+				messages: this.messages,
+			} satisfies Message),
+		);
+	}
+
+	saveMessage(message: ChatMessage) {
+		const existingMessage = this.messages.find((m) => m.id === message.id);
+		if (existingMessage) {
+			this.messages = this.messages.map((m) => {
+				if (m.id === message.id) {
+					return message;
+				}
+				return m;
+			});
+		} else {
+			this.messages.push(message);
+		}
+
+		this.ctx.storage.sql.exec(
+			`INSERT INTO messages (id, user, role, content) VALUES (?, ?, ?, ?)
+			 ON CONFLICT (id) DO UPDATE SET content = ?`,
+			message.id,
+			message.user,
+			message.role,
+			message.content,
+			message.content,
+		);
+	}
+
+	onMessage(connection: Connection, message: WSMessage) {
+		this.broadcast(message);
+
+		const parsed = JSON.parse(message as string) as Message;
+		if (parsed.type === "add" || parsed.type === "update") {
+			this.saveMessage(parsed);
+		}
+	}
+}
+
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+
+		if (url.pathname.startsWith("/auth")) {
+			return authHandler.fetch(request, env);
+		}
+
+		return (
+			(await routePartykitRequest(request, { ...env })) ||
+			env.ASSETS.fetch(request)
+		);
+	},
+} satisfies ExportedHandler<Env>;
